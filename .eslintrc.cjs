e40d4b1350ddbb4e6afd6f15981528976e9baf50
HappyRyan2 <HappyRyan2@Hotmail.com>
2023-09-09 19:03:07 +0100

    setup TypeScript, Mocha + Chai, and ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+	"env": {
+		"node": true,
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/eslint-recommended",
+		"plugin:@typescript-eslint/recommended",
+	],
+	"parserOptions": {
+		"ecmaVersion": 12,
+		"sourceType": "module",
+		"parser": "@typescript-eslint/parser",
+	},
+	"rules": {
+		"quotes": ["error", "double"],
+		"semi": ["error", "always"],
+		"indent": ["error", "tab"],
+		"no-multi-spaces": ["error"],
+		"comma-dangle": ["error", "always-multiline"],
+		"no-mixed-spaces-and-tabs": ["error"],
+		"prefer-template": ["error"],
+		"template-curly-spacing": ["error", "never"],
+		"func-style": ["error", "expression"],
+	},
+	"plugins": [
+		"@typescript-eslint",
+	],
+};
