ec7b869e716fee7dd4b7474435e4db176557f0df
HappyRyan2 <HappyRyan2@Hotmail.com>
2023-09-10 21:35:29 +0100

    added method `App.renderAndUpdate`

--- modules/App.ts
+++ modules/App.ts
@@ -8,15 +8,22 @@ class App {
 	}
 
 	initialize() {
-		
+		this.renderAndUpdate();	
 	}
 
 	render() {
 		const div = document.createElement("div");
+		div.id = "lines-container";
 		for(const line of this.lines) {
 			div.appendChild(line.render());
 		}
 		return div;
+	}
+	renderAndUpdate() {
+		const newDiv = this.render();
+		const oldDiv = document.getElementById("lines-container")!;
+		oldDiv.insertAdjacentElement("afterend", newDiv);
+		oldDiv.remove();
 	}
 }
 
