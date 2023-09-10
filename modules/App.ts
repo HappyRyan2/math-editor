cd48db5d733fd36137702fcc40c94138144154a6
HappyRyan2 <HappyRyan2@Hotmail.com>
2023-09-10 20:38:13 +0100

    added `render` method

--- modules/App.ts
+++ modules/App.ts
@@ -10,6 +10,14 @@ class App {
 	initialize() {
 		
 	}
+
+	render() {
+		const div = document.createElement("div");
+		for(const line of this.lines) {
+			div.appendChild(line.render());
+		}
+		return div;
+	}
 }
 
 const app = new App();
--- modules/Line.ts
+++ modules/Line.ts
@@ -6,4 +6,12 @@ export class Line {
 	constructor(components: MathComponent[]) {
 		this.components = components;
 	}
+
+	render() {
+		const div = document.createElement("div");
+		for(const component of this.components) {
+			div.appendChild(component.render());
+		}
+		return div;
+	}
 }
