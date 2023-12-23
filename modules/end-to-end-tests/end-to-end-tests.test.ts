f41945fdced65dc1673b2ecc3e1fb03bcba076d6
HappyRyan2 <HappyRyan2@Hotmail.com>
2023-12-22 17:24:38 -0800

    improved test coverage
    The integration tests now cover more of the math components, and also use the autocomplete.

--- modules/end-to-end-tests/end-to-end-tests.test.ts
+++ modules/end-to-end-tests/end-to-end-tests.test.ts
@@ -1,7 +1,7 @@
 import { _electron } from "playwright";
 import { test, expect } from "@playwright/test";
 
-test("the user can insert superscripts", async () => {
+test("the user can insert various math components, including using autocomplete", async () => {
 	const electronApp = await _electron.launch({ args: ["compiled/main.js"], env: { CI: "true" } });
 	const page = await electronApp.firstWindow();
 	await page.waitForLoadState();
@@ -12,8 +12,18 @@ test("the user can insert superscripts", async () => {
 	await page.keyboard.press("^");
 	await page.keyboard.up("Shift");
 	await page.keyboard.press("2");
+	await page.keyboard.press("ArrowRight");
+	await page.keyboard.press("+");
+	await page.keyboard.press("1");
+	await page.keyboard.press("/");
+	await page.keyboard.press("3");
+	await page.keyboard.down("Shift");
+	await page.keyboard.press("(");
+	await page.keyboard.up("Shift");
+	await page.keyboard.type("alpha");
+	await page.keyboard.press("Tab");
 	try {
-		await expect(page).toHaveScreenshot("image.png");
+		await expect(page).toHaveScreenshot("math-components.png");
 	}
 	finally {
 		electronApp.close();
